cc7a76f8dd5e9ec5b654949dc0af09eb8205639d
Adity Kumar <neelaadityakumar@gmail.com>
2024-07-23 17:00:52 +0530

    add digit & explaination

--- analog-clock/script.js
+++ analog-clock/script.js
@@ -1,25 +1,32 @@
-// script.js
+var second = 0;
+var minute = 0;
+var hour = 0;
+var d = new Date();
+//src https://www.geeksforgeeks.org/puzzle-degrees-between-hand-of-clocks-on-315/
+setInterval(function () {
+  d = new Date();
+  const sec = d.getSeconds(),
+    min = d.getMinutes(),
+    hr = d.getHours();
+  secondDegree = sec * (360 / 60);
+  minuteDegree = min * (360 / 60) + Math.round(sec * 0.1);
+  //Each second corresponds to 0.1 degrees of minute hand movement because:
 
-const hourHand = document.querySelector(".hour-hand");
-const minuteHand = document.querySelector(".minute-hand");
-const secondHand = document.querySelector(".second-hand");
+  //  60 minute = 360 degree
+  //  3600 seconds = 360 degree
+  //  1 seconds = 0.1 degree
 
-function setDate() {
-  const now = new Date();
+  hourDegree = hr * 30 + Math.round(min * 0.5) + Math.round((sec * 0.5) / 12);
 
-  const seconds = now.getSeconds();
-  const secondsDegrees = (seconds / 60) * 360 + 90; // +90 to offset initial position
-  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-
-  const minutes = now.getMinutes();
-  const minutesDegrees = (minutes / 60) * 360 + (seconds / 60) * 6 + 90; // +90 to offset initial position
-  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
-
-  const hours = now.getHours();
-  const hoursDegrees = (hours / 12) * 360 + (minutes / 60) * 30 + 90; // +90 to offset initial position
-  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
-}
-
-setInterval(setDate, 1000);
-
-setDate(); // Initial call to set the clock hands correctly at the start
+  //Each hour corresponds to 0.5 degrees of hour hand movement because:
+  //  12 hours = 360 degree
+  //  720 minute = 360 degree
+  //  1 minute = 0.5 degree
+  //  1 sec= 0.5/12 degree
+  document.getElementById("second-hand").style.transform =
+    "rotate(" + secondDegree + "deg)";
+  document.getElementById("minute-hand").style.transform =
+    "rotate(" + minuteDegree + "deg)";
+  document.getElementById("hour-hand").style.transform =
+    "rotate(" + hourDegree + "deg)";
+}, 1000);
